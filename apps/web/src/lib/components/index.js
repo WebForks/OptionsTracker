1d92f78bdff5084fb1a8969c238e7d73b51162bf
WebForks <35718739+a3eon-1@users.noreply.github.com>
2023-10-12 16:47:19 -0700

    inital commit

---
+++ apps/web/src/lib/components/index.js
@@ -0,0 +1,3 @@
+import Input from './Input.svelte';
+import TextArea from './TextArea.svelte';
+export { Input, TextArea };
---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    let name = '';
+</script>
+
+<input type="text" bind:value={name} />
+
+<h1>Hello {name}!</h1>
+
+{#if name}
+    <div>hello</div>
+{/if}
